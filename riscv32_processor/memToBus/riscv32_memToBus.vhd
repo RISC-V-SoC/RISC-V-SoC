d1db8d259973208600960d88a7fdb6c51080c36f
Jacko Dirks <jacko.dirks@gmail.com>
2024-11-15 17:48:56 +0100

    riscv32_memToBus: Remove unneeded signals

--- riscv32_processor/memToBus/riscv32_memToBus.vhd
+++ riscv32_processor/memToBus/riscv32_memToBus.vhd
@@ -43,7 +43,6 @@ architecture behaviourial of riscv32_memToBus is
     type state_type is (idle, cached_read_busy, uncached_read_busy, cached_write_busy, uncached_write_busy, cache_flushing);
     signal current_state : state_type := idle;
     signal next_state : state_type := idle;
-    signal state_forces_stall : boolean := false;
 
     signal address_in_dcache_range : boolean;
     signal cached_read_required : boolean;
@@ -54,15 +53,14 @@ architecture behaviourial of riscv32_memToBus is
 
     signal fsm_write_busy : boolean := false;
 
-    signal interactor_busy : boolean;
     signal interactor_completed : boolean;
     signal interactor_fault : boolean;
     signal interactor_doWrite : boolean := false;
     signal interactor_doRead : boolean := false;
     signal interactor_dataOut : bus_data_type;
     signal interactor_dataIn : bus_data_type;
-    signal interactor_should_write_cached : boolean := false;
-    signal interactor_should_write_uncached : boolean := false;
+    signal cached_write_required : boolean := false;
+    signal uncached_write_required : boolean := false;
     signal interactor_writeAddress : bus_address_type;
     signal interactor_writeByteMask : bus_byte_mask_type;
     signal interactor_readByteMask : bus_byte_mask_type;
@@ -77,10 +75,8 @@ architecture behaviourial of riscv32_memToBus is
     signal cache_reset : boolean := false;
 
     signal volatile_cache_data : bus_data_type;
-    signal volatile_read_cache_valid : boolean := false;
-    signal volatile_write_cache_valid : boolean := false;
-    signal volatile_cache_update_read : boolean;
-    signal volatile_cache_update_write : boolean;
+    signal volatile_cache_valid : boolean := false;
+    signal volatile_cache_update : boolean;
 
     signal cache_flush_allowed : boolean := false;
     signal cache_flush_required : boolean := false;
@@ -94,87 +90,75 @@ begin
     byteMask_indicates_full_word <= byteMask = "1111";
     cache_flush_busy <= cache_flush_required;
 
-    state_machine_output : process (current_state, cache_miss, interactor_completed, doRead, cached_read_required, uncached_read_required, interactor_should_write_cached, interactor_should_write_uncached, cache_flush_required, cache_flush_write_required)
+    state_machine_output : process (current_state, cache_miss, interactor_completed, doRead, cached_read_required, uncached_read_required, cached_write_required, uncached_write_required, cache_flush_required, cache_flush_write_required)
     begin
         next_state <= current_state;
         case current_state is
             when idle =>
-                state_forces_stall <= false;
                 cache_doWrite_fromBus <= false;
-                interactor_doWrite <= interactor_should_write_cached or interactor_should_write_uncached;
+                interactor_doWrite <= cached_write_required or uncached_write_required;
                 interactor_doRead <= cached_read_required or uncached_read_required;
-                volatile_cache_update_read <= false;
-                volatile_cache_update_write <= false;
+                volatile_cache_update <= false;
                 fsm_write_busy <= false;
                 cache_flush_allowed <= false;
                 if cached_read_required then
                     next_state <= cached_read_busy;
                 elsif uncached_read_required then
                     next_state <= uncached_read_busy;
-                elsif interactor_should_write_uncached then
+                elsif uncached_write_required then
                     next_state <= uncached_write_busy;
-                elsif interactor_should_write_cached then
+                elsif cached_write_required then
                     next_state <= cached_write_busy;
                 elsif cache_flush_required then
                     next_state <= cache_flushing;
                 end if;
             when cached_read_busy =>
-                state_forces_stall <= true;
                 cache_doWrite_fromBus <= interactor_completed;
-                interactor_doWrite <= interactor_should_write_cached;
+                interactor_doWrite <= cached_write_required;
                 interactor_doRead <= false;
-                volatile_cache_update_read <= false;
-                volatile_cache_update_write <= false;
+                volatile_cache_update <= false;
                 fsm_write_busy <= false;
                 cache_flush_allowed <= false;
-                if interactor_completed and interactor_should_write_cached then
+                if interactor_completed and cached_write_required then
                     next_state <= cached_write_busy;
                 elsif interactor_completed then
                     next_state <= idle;
                 end if;
             when uncached_read_busy =>
-                state_forces_stall <= true;
                 cache_doWrite_fromBus <= false;
                 interactor_doWrite <= false;
                 interactor_doRead <= false;
-                volatile_cache_update_read <= interactor_completed;
-                volatile_cache_update_write <= false;
+                volatile_cache_update <= interactor_completed;
                 fsm_write_busy <= false;
                 cache_flush_allowed <= false;
                 if interactor_completed then
                     next_state <= idle;
                 end if;
             when cached_write_busy =>
-                state_forces_stall <= false;
                 cache_doWrite_fromBus <= false;
                 interactor_doWrite <= false;
                 interactor_doRead <= false;
-                volatile_cache_update_read <= false;
-                volatile_cache_update_write <= false;
+                volatile_cache_update <= false;
                 fsm_write_busy <= true;
                 cache_flush_allowed <= false;
                 if interactor_completed then
                     next_state <= idle;
                 end if;
             when uncached_write_busy =>
-                state_forces_stall <= true;
                 cache_doWrite_fromBus <= false;
                 interactor_doWrite <= false;
                 interactor_doRead <= false;
-                volatile_cache_update_read <= false;
-                volatile_cache_update_write <= interactor_completed;
+                volatile_cache_update <= interactor_completed;
                 fsm_write_busy <= true;
                 cache_flush_allowed <= false;
                 if interactor_completed then
                     next_state <= idle;
                 end if;
             when cache_flushing =>
-                state_forces_stall <= true;
                 cache_doWrite_fromBus <= false;
                 interactor_doWrite <= cache_flush_write_required;
                 interactor_doRead <= false;
-                volatile_cache_update_read <= false;
-                volatile_cache_update_write <= false;
+                volatile_cache_update <= false;
                 fsm_write_busy <= false;
                 cache_flush_allowed <= true;
                 if not cache_flush_required then
@@ -194,26 +178,24 @@ begin
         end if;
     end process;
 
-    determine_stallout : process(cache_miss, doRead, state_forces_stall, address_in_dcache_range, volatile_read_cache_valid, interactor_should_write_cached, fsm_write_busy, flush_cache, cache_flush_required, interactor_should_write_uncached, byteMask_indicates_full_word, doWrite)
+    determine_stallout : process(cache_miss, doRead, address_in_dcache_range, volatile_cache_valid, cached_write_required, fsm_write_busy, flush_cache, cache_flush_required, uncached_write_required, byteMask_indicates_full_word, doWrite)
     begin
         stallOut <= false;
-        if state_forces_stall then
-            stallOut <= true;
-        elsif flush_cache or cache_flush_required then
+        if flush_cache or cache_flush_required then
             stallOut <= true;
         elsif doRead then
             if address_in_dcache_range then
                 stallOut <= cache_miss;
             else
-                stallOut <= not volatile_read_cache_valid;
+                stallOut <= not volatile_cache_valid;
             end if;
         elsif doWrite then
             if address_in_dcache_range and byteMask_indicates_full_word then
-                stallOut <= interactor_should_write_cached and fsm_write_busy;
+                stallOut <= cached_write_required and fsm_write_busy;
             elsif address_in_dcache_range and not byteMask_indicates_full_word then
                 stallOut <= cache_miss;
             else
-                stallOut <= interactor_should_write_uncached;
+                stallOut <= uncached_write_required;
             end if;
         end if;
     end process;
@@ -222,19 +204,14 @@ begin
     begin
         if rising_edge(clk) then
             if rst then
-                volatile_read_cache_valid <= false;
-                volatile_write_cache_valid <= false;
+                volatile_cache_valid <= false;
                 hasFault <= false;
-            elsif volatile_cache_update_read then
+            elsif volatile_cache_update then
                 volatile_cache_data <= interactor_dataOut;
-                volatile_read_cache_valid <= true;
-                hasFault <= interactor_fault;
-            elsif volatile_cache_update_write then
-                volatile_write_cache_valid <= true;
+                volatile_cache_valid <= true;
                 hasFault <= interactor_fault;
             elsif not stallIn then
-                volatile_read_cache_valid <= false;
-                volatile_write_cache_valid <= false;
+                volatile_cache_valid <= false;
                 hasFault <= false;
             end if;
         end if;
@@ -249,11 +226,11 @@ begin
         end if;
     end process;
 
-    command_and_control : process(byteMask_indicates_full_word, cache_miss, cache_line_dirty, doWrite, doRead, cache_reconstructedAddr, cache_dataOut, dataIn, address, volatile_write_cache_valid, fsm_write_busy, address_in_dcache_range, cache_flush_reconstructedAddr, cache_flush_line_data_out, cache_flush_allowed, byteMask, volatile_read_cache_valid)
+    command_and_control : process(byteMask_indicates_full_word, cache_miss, cache_line_dirty, doWrite, doRead, cache_reconstructedAddr, cache_dataOut, dataIn, address, volatile_cache_valid, fsm_write_busy, address_in_dcache_range, cache_flush_reconstructedAddr, cache_flush_line_data_out, cache_flush_allowed, byteMask)
     begin
         -- Cover all default cases
-        interactor_should_write_cached <= false;
-        interactor_should_write_uncached <= false;
+        cached_write_required <= false;
+        uncached_write_required <= false;
         cache_doWrite_fromProc <= false;
         interactor_writeByteMask <= byteMask;
         interactor_dataIn <= dataIn;
@@ -262,8 +239,8 @@ begin
         cached_read_required <= false;
         uncached_read_required <= false;
         if cache_flush_allowed then
-            interactor_should_write_cached <= false;
-            interactor_should_write_uncached <= false;
+            cached_write_required <= false;
+            uncached_write_required <= false;
             cache_doWrite_fromProc <= false;
             interactor_writeByteMask <= (others => '1');
             interactor_dataIn <= cache_flush_line_data_out;
@@ -274,8 +251,8 @@ begin
         elsif doWrite and address_in_dcache_range then
             -- Proc writes full word to a clean cache line, memory requires no update
             if byteMask_indicates_full_word and not cache_line_dirty then
-                interactor_should_write_cached <= false;
-                interactor_should_write_uncached <= false;
+                cached_write_required <= false;
+                uncached_write_required <= false;
                 cache_doWrite_fromProc <= true;
                 interactor_writeByteMask <= byteMask;
                 interactor_dataIn <= dataIn;
@@ -285,8 +262,8 @@ begin
                 uncached_read_required <= false;
             -- Proc writes data to cached word, just update the cache
             elsif not cache_miss then
-                interactor_should_write_cached <= false;
-                interactor_should_write_uncached <= false;
+                cached_write_required <= false;
+                uncached_write_required <= false;
                 cache_doWrite_fromProc <= true;
                 interactor_writeByteMask <= byteMask;
                 interactor_dataIn <= dataIn;
@@ -296,8 +273,8 @@ begin
                 uncached_read_required <= false;
             -- Proc overwrites existing and dirty cache line. Dirty cache line needs to be forwarded to memory
             elsif byteMask_indicates_full_word and cache_miss and cache_line_dirty then
-                interactor_should_write_cached <= true;
-                interactor_should_write_uncached <= false;
+                cached_write_required <= true;
+                uncached_write_required <= false;
                 cache_doWrite_fromProc <= not fsm_write_busy;
                 interactor_writeByteMask <= (others => '1');
                 interactor_dataIn <= cache_dataOut;
@@ -307,8 +284,8 @@ begin
                 uncached_read_required <= false;
             -- Proc writes partial data to uncached word, we read the entire word from memory
             elsif not byteMask_indicates_full_word and cache_miss and not cache_line_dirty then
-                interactor_should_write_cached <= false;
-                interactor_should_write_uncached <= false;
+                cached_write_required <= false;
+                uncached_write_required <= false;
                 cache_doWrite_fromProc <= false;
                 interactor_writeByteMask <= (others => '1');
                 interactor_dataIn <= dataIn;
@@ -318,8 +295,8 @@ begin
                 uncached_read_required <= false;
             -- Proc writes partial data to uncached word, we read the entire word from memory and store the dirty word to memory
             elsif not byteMask_indicates_full_word and cache_miss and cache_line_dirty then
-                interactor_should_write_cached <= true;
-                interactor_should_write_uncached <= false;
+                cached_write_required <= true;
+                uncached_write_required <= false;
                 cache_doWrite_fromProc <= false;
                 interactor_writeByteMask <= (others => '1');
                 interactor_dataIn <= cache_dataOut;
@@ -329,8 +306,8 @@ begin
                 uncached_read_required <= false;
             end if;
         elsif doWrite and not address_in_dcache_range then
-            interactor_should_write_cached <= false;
-            interactor_should_write_uncached <= not volatile_write_cache_valid;
+            cached_write_required <= false;
+            uncached_write_required <= not volatile_cache_valid;
             cache_doWrite_fromProc <= false;
             interactor_writeByteMask <= byteMask;
             interactor_dataIn <= dataIn;
@@ -341,8 +318,8 @@ begin
         elsif doRead and address_in_dcache_range then
             -- Dirty line needs to be comitted to memory
             if cache_miss and cache_line_dirty then
-                interactor_should_write_cached <= true;
-                interactor_should_write_uncached <= false;
+                cached_write_required <= true;
+                uncached_write_required <= false;
                 cache_doWrite_fromProc <= false;
                 interactor_writeByteMask <= (others => '1');
                 interactor_dataIn <= cache_dataOut;
@@ -351,8 +328,8 @@ begin
                 cached_read_required <= true;
                 uncached_read_required <= false;
             elsif cache_miss then
-                interactor_should_write_cached <= false;
-                interactor_should_write_uncached <= false;
+                cached_write_required <= false;
+                uncached_write_required <= false;
                 cache_doWrite_fromProc <= false;
                 interactor_writeByteMask <= (others => '1');
                 interactor_dataIn <= (others => '0');
@@ -362,15 +339,15 @@ begin
                 uncached_read_required <= false;
             end if;
         elsif doRead and not address_in_dcache_range then
-            interactor_should_write_cached <= false;
-            interactor_should_write_uncached <= false;
+            cached_write_required <= false;
+            uncached_write_required <= false;
             cache_doWrite_fromProc <= false;
             interactor_writeByteMask <= (others => '1');
             interactor_dataIn <= (others => '0');
             interactor_writeAddress <= address;
             interactor_readByteMask <= byteMask;
             cached_read_required <= false;
-            uncached_read_required <= not volatile_read_cache_valid;
+            uncached_read_required <= not volatile_cache_valid;
         end if;
     end process;
 
@@ -461,7 +438,6 @@ begin
         doWrite => interactor_doWrite,
         dataIn => interactor_dataIn,
 
-        busy => interactor_busy,
         completed => interactor_completed,
         fault => interactor_fault,
         dataOut => interactor_dataOut
