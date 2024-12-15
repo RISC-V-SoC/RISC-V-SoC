5754ef9793eb085d3ec056f4b87b922dbbb442d0
Jacko Dirks <jacko.dirks@gmail.com>
2024-12-15 11:45:20 +0100

    Introduce bus_cache_director

---
+++ bus/bus_cache/bus_cache_director.vhd
@@ -0,0 +1,169 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+library work;
+use work.bus_pkg.all;
+
+entity bus_cache_director is
+    generic (
+        words_per_line_log2b : natural range 0 to natural'high;
+        total_line_count_log2b : natural range 1 to natural'high;
+        bank_count_log2b : natural range 1 to natural'high
+    );
+    port (
+        clk : in std_logic;
+        rst : in boolean;
+
+        address : in bus_aligned_address_type;
+        line_index : in natural range 0 to 2**total_line_count_log2b - 1;
+        index_mode : in boolean;
+
+        word_index_from_frontend : in natural range 0 to 2**words_per_line_log2b - 1;
+        data_from_frontend : in bus_data_type;
+        bytemask_from_frontend : in bus_byte_mask_type;
+        data_to_frontend : out bus_data_type;
+        do_write_from_frontend : in boolean;
+        do_read_from_frontend : in boolean;
+
+        word_index_from_backend : in natural range 0 to 2**words_per_line_log2b - 1;
+        data_from_backend : in bus_data_type;
+        data_to_backend : out bus_data_type;
+        do_write_from_backend : in boolean;
+        mark_line_clean : in boolean;
+
+        reconstructed_address : out bus_aligned_address_type;
+
+        hit : out boolean;
+        dirty : out boolean
+    );
+end entity;
+
+architecture behaviourial of bus_cache_director is
+    constant words_per_line : natural := 2**words_per_line_log2b;
+    constant total_line_count : natural := 2**total_line_count_log2b;
+    constant bank_count : natural := 2**bank_count_log2b;
+
+    constant line_count_log2b : natural := total_line_count_log2b - bank_count_log2b;
+    constant max_age : natural := 2**bank_count_log2b - 1;
+
+    type age_array_type is array(0 to bank_count - 1) of natural range 0 to max_age;
+
+    signal bank_line_index : natural range 0 to 2**line_count_log2b - 1;
+    signal bank_index : natural range 0 to bank_count - 1;
+
+    signal data_to_frontend_array : bus_data_array(0 to bank_count - 1);
+    signal do_write_from_frontend_array : boolean_vector(0 to bank_count - 1);
+    signal data_to_backend_array : bus_data_array(0 to bank_count - 1);
+    signal do_write_from_backend_array : boolean_vector(0 to bank_count - 1);
+    signal mark_line_clean_array : boolean_vector(0 to bank_count - 1);
+    signal reset_age_array : boolean_vector(0 to bank_count - 1);
+    signal increment_age_array : boolean_vector(0 to bank_count - 1);
+    signal miss_array : boolean_vector(0 to bank_count - 1);
+    signal dirty_array : boolean_vector(0 to bank_count - 1);
+    signal age_array : age_array_type;
+    signal reconstructed_address_array : bus_aligned_address_array(0 to bank_count - 1);
+
+    signal hit_index : natural range 0 to bank_count - 1;
+    signal evict_index : natural range 0 to bank_count - 1;
+
+    signal hit_age : natural range 0 to max_age;
+begin
+    assert(total_line_count_log2b > bank_count_log2b);
+
+    bank_line_index <= line_index rem 2**line_count_log2b;
+    bank_index <= line_index / 2**line_count_log2b;
+
+    hit <= not miss_array(hit_index);
+    data_to_frontend <= data_to_frontend_array(hit_index);
+    hit_age <= age_array(hit_index);
+
+    index_handling : process(index_mode, evict_index, bank_index, data_to_backend_array, reconstructed_address_array, dirty_array)
+    begin
+        if index_mode then
+            data_to_backend <= data_to_backend_array(bank_index);
+            reconstructed_address <= reconstructed_address_array(bank_index);
+            dirty <= dirty_array(bank_index);
+        else
+            data_to_backend <= data_to_backend_array(evict_index);
+            reconstructed_address <= reconstructed_address_array(evict_index);
+            dirty <= dirty_array(evict_index);
+        end if;
+    end process;
+
+    hit_handling : process(miss_array)
+    begin
+        hit_index <= 0;
+        for i in 0 to bank_count - 1 loop
+            if not miss_array(i) then
+                hit_index <= i;
+            end if;
+        end loop;
+    end process;
+
+    evict_handling : process(age_array)
+    begin
+        evict_index <= 0;
+        for i in bank_count - 1 downto 0 loop
+            if age_array(i) = max_age then
+                evict_index <= i;
+            end if;
+        end loop;
+    end process;
+
+    write_command_handling : process(hit_index, evict_index, do_write_from_frontend, do_write_from_backend, mark_line_clean)
+    begin
+        for i in 0 to bank_count - 1 loop
+            do_write_from_frontend_array(i) <= do_write_from_frontend and i = hit_index;
+            do_write_from_backend_array(i) <= do_write_from_backend and i = evict_index;
+            mark_line_clean_array(i) <= mark_line_clean and i = evict_index;
+        end loop;
+    end process;
+
+    age_handling : process(do_read_from_frontend, do_write_from_frontend, hit_index, hit_age, age_array)
+        variable bank_operation : boolean;
+    begin
+        bank_operation := do_read_from_frontend or do_write_from_frontend;
+        for i in 0 to bank_count - 1 loop
+            reset_age_array(i) <= bank_operation and i = hit_index;
+            increment_age_array(i) <= bank_operation and i /= hit_index and age_array(i) <= hit_age;
+        end loop;
+    end process;
+
+
+    banks : for index in 0 to bank_count - 1 generate
+        bank : entity work.bus_cache_bank
+        generic map (
+            words_per_line_log2b => words_per_line_log2b,
+            line_count_log2b => line_count_log2b,
+            max_age => max_age
+        ) port map (
+            clk => clk,
+            rst => rst,
+
+            address => address,
+            line_index => bank_line_index,
+            index_mode => index_mode,
+
+            word_index_from_frontend => word_index_from_frontend,
+            data_from_frontend => data_from_frontend,
+            bytemask_from_frontend => bytemask_from_frontend,
+            data_to_frontend => data_to_frontend_array(index),
+            do_write_from_frontend => do_write_from_frontend_array(index),
+
+            word_index_from_backend => word_index_from_backend,
+            data_from_backend => data_from_backend,
+            data_to_backend => data_to_backend_array(index),
+            do_write_from_backend => do_write_from_backend_array(index),
+            mark_line_clean => mark_line_clean_array(index),
+
+            reset_age => reset_age_array(index),
+            increase_age => increment_age_array(index),
+
+            miss => miss_array(index),
+            dirty => dirty_array(index),
+            age => age_array(index),
+            reconstructed_address => reconstructed_address_array(index)
+        );
+    end generate;
+end architecture;
---
+++ bus/bus_cache/test/bus_cache_director_tb.vhd
@@ -0,0 +1,329 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.std_logic_misc.all;
+
+library vunit_lib;
+context vunit_lib.vunit_context;
+context vunit_lib.vc_context;
+
+library src;
+use src.bus_pkg.all;
+
+entity bus_cache_director_tb is
+    generic (
+        runner_cfg : string);
+end entity;
+
+architecture tb of bus_cache_director_tb is
+    constant clk_period : time := 20 ns;
+    constant words_per_line_log2b : natural := 1;
+    constant total_line_count_log2b : natural := 3;
+    constant bank_count_log2b : natural := 2;
+
+    constant words_per_line : natural := 2**words_per_line_log2b;
+    constant total_line_count : natural := 2**total_line_count_log2b;
+    constant bank_count : natural := 2**bank_count_log2b;
+
+    signal clk : std_logic := '0';
+    signal rst : boolean := false;
+
+    signal address : bus_aligned_address_type := (others => '0');
+    signal line_index : natural := 0;
+    signal index_mode : boolean := false;
+
+    signal word_index_from_frontend : natural := 0;
+    signal data_from_frontend : bus_data_type := (others => '0');
+    signal bytemask_from_frontend : bus_byte_mask_type := (others => '0');
+    signal data_to_frontend : bus_data_type;
+    signal do_write_from_frontend : boolean := false;
+    signal do_read_from_frontend : boolean := false;
+
+    signal word_index_from_backend : natural := 0;
+    signal data_from_backend : bus_data_type := (others => '0');
+    signal data_to_backend : bus_data_type;
+    signal do_write_from_backend : boolean := false;
+    signal mark_line_clean : boolean := false;
+
+    signal reconstructed_address : bus_aligned_address_type;
+
+    signal hit : boolean;
+    signal dirty : boolean;
+begin
+    clk <= not clk after (clk_period/2);
+
+    main : process
+    begin
+        test_runner_setup(runner, runner_cfg);
+        while test_suite loop
+            if run("Simulate read on invalid line") then
+                address <= std_logic_vector(to_unsigned(words_per_line * 0, address'length));
+                word_index_from_frontend <= 0;
+                wait until rising_edge(clk);
+                wait until falling_edge(clk);
+                check_false(hit);
+                check_false(dirty);
+                for i in 0 to words_per_line-1 loop
+                    word_index_from_backend <= i;
+                    data_from_backend <= std_logic_vector(to_unsigned(i, data_from_backend'length));
+                    do_write_from_backend <= true;
+                    if i = words_per_line-1 then
+                        mark_line_clean <= true;
+                    end if;
+                    wait until rising_edge(clk);
+                    mark_line_clean <= false;
+                end loop;
+                do_write_from_backend <= false;
+                wait until rising_edge(clk);
+                wait until falling_edge(clk);
+                check_true(hit);
+                check(or_reduce(data_to_frontend) = '0');
+                do_read_from_frontend <= true;
+                wait until rising_edge(clk);
+                do_read_from_frontend <= false;
+                wait until rising_edge(clk);
+            elsif run("Cache two colliding lines") then
+                address <= std_logic_vector(to_unsigned(0, address'length));
+                word_index_from_frontend <= 0;
+                wait until rising_edge(clk);
+                wait until falling_edge(clk);
+                check_false(hit);
+                check_false(dirty);
+                for i in 0 to words_per_line-1 loop
+                    word_index_from_backend <= i;
+                    data_from_backend <= std_logic_vector(to_unsigned(i, data_from_backend'length));
+                    do_write_from_backend <= true;
+                    if i = words_per_line-1 then
+                        mark_line_clean <= true;
+                    end if;
+                    wait until rising_edge(clk);
+                    mark_line_clean <= false;
+                end loop;
+                do_write_from_backend <= false;
+                wait until rising_edge(clk);
+                do_read_from_frontend <= true;
+                word_index_from_frontend <= 0;
+                wait until rising_edge(clk);
+                do_read_from_frontend <= false;
+                address <= std_logic_vector(to_unsigned(total_line_count * words_per_line, address'length));
+                word_index_from_frontend <= 0;
+                wait until rising_edge(clk);
+                wait until falling_edge(clk);
+                check_false(hit);
+                check_false(dirty);
+                for i in 0 to words_per_line-1 loop
+                    word_index_from_backend <= i;
+                    data_from_backend <= std_logic_vector(to_unsigned(i + 4, data_from_backend'length));
+                    do_write_from_backend <= true;
+                    if i = words_per_line-1 then
+                        mark_line_clean <= true;
+                    end if;
+                    wait until rising_edge(clk);
+                    mark_line_clean <= false;
+                end loop;
+                do_write_from_backend <= false;
+                address <= std_logic_vector(to_unsigned(0, address'length));
+                word_index_from_frontend <= 0;
+                wait until rising_edge(clk);
+                wait until falling_edge(clk);
+                check_true(hit);
+                check(or_reduce(data_to_frontend) = '0');
+            elsif run("Oldest line is evicted first") then
+                for bank_index in 0 to bank_count - 1 loop
+                    address <= std_logic_vector(to_unsigned((bank_count * words_per_line)*bank_index, address'length));
+                    wait until rising_edge(clk);
+                    for i in 0 to words_per_line-1 loop
+                        word_index_from_backend <= i;
+                        data_from_backend <= std_logic_vector(to_unsigned(i + (bank_index * bank_count), data_from_backend'length));
+                        do_write_from_backend <= true;
+                        if i = words_per_line-1 then
+                            mark_line_clean <= true;
+                        end if;
+                        wait until rising_edge(clk);
+                        mark_line_clean <= false;
+                    end loop;
+                    do_write_from_backend <= false;
+                    wait until rising_edge(clk);
+                    word_index_from_frontend <= 0;
+                    do_read_from_frontend <= true;
+                    wait until rising_edge(clk);
+                    do_read_from_frontend <= false;
+                end loop;
+                address <= std_logic_vector(to_unsigned((bank_count * words_per_line)*bank_count, address'length));
+                wait until rising_edge(clk);
+                for i in 0 to words_per_line-1 loop
+                    word_index_from_backend <= i;
+                    data_from_backend <= std_logic_vector(to_unsigned(i + (bank_count * bank_count), data_from_backend'length));
+                    do_write_from_backend <= true;
+                    wait until rising_edge(clk);
+                end loop;
+                do_write_from_backend <= false;
+                address <= std_logic_vector(to_unsigned(0, address'length));
+                wait until rising_edge(clk);
+                wait until falling_edge(clk);
+                check_false(hit);
+            elsif run("Write also ages bank") then
+                for bank_index in 0 to bank_count - 1 loop
+                    address <= std_logic_vector(to_unsigned((bank_count * words_per_line)*bank_index, address'length));
+                    wait until rising_edge(clk);
+                    for i in 0 to words_per_line-1 loop
+                        word_index_from_backend <= i;
+                        data_from_backend <= std_logic_vector(to_unsigned(i + (bank_index * bank_count), data_from_backend'length));
+                        do_write_from_backend <= true;
+                        if i = words_per_line-1 then
+                            mark_line_clean <= true;
+                        end if;
+                        wait until rising_edge(clk);
+                    end loop;
+                    do_write_from_backend <= false;
+                    wait until rising_edge(clk);
+                    word_index_from_frontend <= 0;
+                    do_write_from_frontend <= true;
+                    data_from_frontend <= std_logic_vector(to_unsigned(0, data_from_frontend'length));
+                    wait until rising_edge(clk);
+                    do_write_from_frontend <= false;
+                end loop;
+                address <= std_logic_vector(to_unsigned((bank_count * words_per_line)*bank_count, address'length));
+                wait until rising_edge(clk);
+                for i in 0 to words_per_line-1 loop
+                    word_index_from_backend <= i;
+                    data_from_backend <= std_logic_vector(to_unsigned(i + (bank_count * bank_count), data_from_backend'length));
+                    do_write_from_backend <= true;
+                    wait until rising_edge(clk);
+                end loop;
+                do_write_from_backend <= false;
+                address <= std_logic_vector(to_unsigned(0, address'length));
+                wait until rising_edge(clk);
+                wait until falling_edge(clk);
+                check_false(hit);
+            elsif run("Test index mode") then
+                for index in 0 to total_line_count - 1 loop
+                    address <= std_logic_vector(to_unsigned(index * words_per_line, address'length));
+                    wait until rising_edge(clk);
+                    for i in 0 to words_per_line-1 loop
+                        word_index_from_backend <= i;
+                        data_from_backend <= std_logic_vector(to_unsigned(index, data_from_backend'length));
+                        do_write_from_backend <= true;
+                        if i = words_per_line-1 then
+                            mark_line_clean <= true;
+                        end if;
+                        wait until rising_edge(clk);
+                        mark_line_clean <= false;
+                    end loop;
+                    do_write_from_backend <= false;
+                    wait until rising_edge(clk);
+                    word_index_from_frontend <= 0;
+                    do_read_from_frontend <= index rem 2 = 0;
+                    do_write_from_frontend <= index rem 2 /= 0;
+                    data_from_frontend <= std_logic_vector(to_unsigned(1000 + index, data_from_frontend'length));
+                    bytemask_from_frontend <= (others => '1');
+                    wait until rising_edge(clk);
+                    do_read_from_frontend <= false;
+                    do_write_from_frontend <= false;
+                end loop;
+                wait until rising_edge(clk);
+                index_mode <= true;
+                word_index_from_backend <= 0;
+                for index in 0 to total_line_count - 1 loop
+                    line_index <= index;
+                    wait until rising_edge(clk);
+                    wait until falling_edge(clk);
+                    check_equal(reconstructed_address, std_logic_vector(to_unsigned(index * words_per_line, reconstructed_address'length)));
+                    check(dirty = (index rem 2 /= 0));
+                    if index rem 2 = 0 then
+                        check_equal(data_to_backend, std_logic_vector(to_unsigned(index, data_to_frontend'length)));
+                    else
+                        check_equal(data_to_backend, std_logic_vector(to_unsigned(1000 + index, data_to_frontend'length)));
+                    end if;
+                end loop;
+            elsif run("Lines older than the current line do not age") then
+                for bank_index in 0 to 1 loop
+                    address <= std_logic_vector(to_unsigned((bank_count * words_per_line)*bank_index, address'length));
+                    wait until rising_edge(clk);
+                    for i in 0 to words_per_line-1 loop
+                        word_index_from_backend <= i;
+                        data_from_backend <= std_logic_vector(to_unsigned(i + (bank_index * bank_count), data_from_backend'length));
+                        do_write_from_backend <= true;
+                        if i = words_per_line-1 then
+                            mark_line_clean <= true;
+                        end if;
+                        wait until rising_edge(clk);
+                        mark_line_clean <= false;
+                    end loop;
+                    do_write_from_backend <= false;
+                    wait until rising_edge(clk);
+                    word_index_from_frontend <= 0;
+                    do_read_from_frontend <= true;
+                    wait until rising_edge(clk);
+                    do_read_from_frontend <= false;
+                end loop;
+                wait until rising_edge(clk);
+                for i in 0 to 5 loop
+                do_read_from_frontend <= true;
+                wait until rising_edge(clk);
+                do_read_from_frontend <= false;
+                end loop;
+
+                for bank_index in 2 to bank_count - 1 loop
+                    address <= std_logic_vector(to_unsigned((bank_count * words_per_line)*bank_index, address'length));
+                    wait until rising_edge(clk);
+                    for i in 0 to words_per_line-1 loop
+                        word_index_from_backend <= i;
+                        data_from_backend <= std_logic_vector(to_unsigned(i + (bank_index * bank_count), data_from_backend'length));
+                        do_write_from_backend <= true;
+                        if i = words_per_line-1 then
+                            mark_line_clean <= true;
+                        end if;
+                        wait until rising_edge(clk);
+                        mark_line_clean <= false;
+                    end loop;
+                    do_write_from_backend <= false;
+                    wait until rising_edge(clk);
+                    word_index_from_frontend <= 0;
+                    do_read_from_frontend <= true;
+                    wait until rising_edge(clk);
+                    do_read_from_frontend <= false;
+                end loop;
+
+                address <= std_logic_vector(to_unsigned(words_per_line * 0, address'length));
+                word_index_from_frontend <= 0;
+                wait until rising_edge(clk);
+                wait until falling_edge(clk);
+                check_true(hit);
+            end if;
+        end loop;
+        wait until rising_edge(clk);
+        wait until falling_edge(clk);
+        test_runner_cleanup(runner);
+        wait;
+    end process;
+
+    test_runner_watchdog(runner,  10 us);
+
+    cache_director : entity src.bus_cache_director
+    generic map (
+        words_per_line_log2b => words_per_line_log2b,
+        total_line_count_log2b => total_line_count_log2b,
+        bank_count_log2b => bank_count_log2b
+    ) port map (
+        clk => clk,
+        rst => rst,
+        address => address,
+        line_index => line_index,
+        index_mode => index_mode,
+        word_index_from_frontend => word_index_from_frontend,
+        data_from_frontend => data_from_frontend,
+        bytemask_from_frontend => bytemask_from_frontend,
+        data_to_frontend => data_to_frontend,
+        do_write_from_frontend => do_write_from_frontend,
+        do_read_from_frontend => do_read_from_frontend,
+        word_index_from_backend => word_index_from_backend,
+        data_from_backend => data_from_backend,
+        data_to_backend => data_to_backend,
+        do_write_from_backend => do_write_from_backend,
+        mark_line_clean => mark_line_clean,
+        reconstructed_address => reconstructed_address,
+        hit => hit,
+        dirty => dirty
+    );
+end architecture;
